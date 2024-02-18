6a35ec115074cdfc24c6f5f0c448410d8ac5dad2
Sebastian Hurtado <juansebastian.hurtadop@gmail.com>
2024-02-18 15:52:32 -0500

    wallet test

--- src/app/shyft-api.service.ts
+++ src/app/shyft-api.service.ts
@@ -12,7 +12,7 @@ export class ShyftApiService {
         if (!publicKey) {
             return of(null);
         }
-        const url = new URL('https://rpc.shyft.to?api_key=773DT6pUIKFQSCob');
+        const url = new URL('https://rpc.shyft.to/sol/v1/wallet/token_balance');
         url.searchParams.set('network', 'mainnet-beta');
         url.searchParams.set('wallet', publicKey);
         url.searchParams.set('token', this._mint);
