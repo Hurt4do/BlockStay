b86075841b6760c2ecca86d966bafe92255449a1
Sebastian Hurtado <juansebastian.hurtadop@gmail.com>
2024-02-18 15:40:22 -0500

    wallet test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
 
 
 @Component({
@@ -8,15 +12,25 @@ import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-materia
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'hdtest1-root',
   template: `    
-    
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
+    <header>    
+      <div style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
       <h1>Inicia sesión</h1>
       <hd-wallet-multi-button></hd-wallet-multi-button>
     </div>
- 
-  `
+    
+      @if (account()){
+        <div>sihay</div>
+      }</header>
+
+  `,
 })
 export class AppComponent {
   private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
 
-}
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
+} 
--- src/app/shyft-api.service.ts
+++ src/app/shyft-api.service.ts
@@ -3,7 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-class ShyftApiService {
+export class ShyftApiService {
     private readonly _httpClient = inject(HttpClient);
     private readonly _header = { 'x-api-key': '773DT6pUIKFQSCob' };
     private readonly _mint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
@@ -12,15 +12,14 @@ class ShyftApiService {
         if (!publicKey) {
             return of(null);
         }
-        const url = new URL('https://devnet-rpc.shyft.to?api_key=773DT6pUIKFQSCob');
+        const url = new URL('https://rpc.shyft.to?api_key=773DT6pUIKFQSCob');
         url.searchParams.set('network', 'mainnet-beta');
         url.searchParams.set('wallet', publicKey);
         url.searchParams.set('token', this._mint);
 
-        return this._httpClient
-            .get<{
-                result: { balance: number; info: { image: string } }
-            }>(url.toString(), { headers: this._header })
+        return this._httpClient.get<{
+            result: { balance: number; info: { image: string } }
+        }>(url.toString(), { headers: this._header })
             .pipe(map((response) => response.result));
     }
 } 
