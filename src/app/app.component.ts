a954507597f440660302a15fb44f75c6fbc0d745
Sebastian Hurtado <juansebastian.hurtadop@gmail.com>
2024-02-18 09:38:05 -0500

    Primer botón

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
-  selector: 'test1-root',
+  imports: [RouterModule, HdWalletMultiButtonComponent],
+  selector: 'hdtest1-root',
   template: `    
-  <div style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
-    <h1>Inicia sesión</h1>
-    <button mat-raised-button>Conecta tu billetera</button>
-  </div>
+    
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
+      <h1>Inicia sesión</h1>
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+    </div>
+ 
   `
 })
 export class AppComponent { }
